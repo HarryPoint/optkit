3a823b5738fada57814161c4a9eedc3b469945de
Huaigang Liang <huaigang.liang@lumens.sg>
2025-05-21 14:23:44 +0800

    chore(demo): update dependencies and clean up styles
    - Updated package.json to include @ant-design/pro-components and antd dependencies.
    - Adjusted devDependencies to ensure compatibility with TypeScript and ESLint.
    - Removed unused CSS variables and styles from globals.css for a cleaner design.
    - Simplified the page component in page.tsx to a basic structure.

---
+++ apps/frontend/demo/src/app/sys/core-components/AsyncButton/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { AsyncButton } from "optkit-toolbox";
+
+export default function Page() {
+  return <AsyncButton>AsyncButton</AsyncButton>;
+}
---
+++ apps/frontend/demo/src/app/sys/layout.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ProLayout } from "@ant-design/pro-components";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <ProLayout>{children}</ProLayout>;
+}
