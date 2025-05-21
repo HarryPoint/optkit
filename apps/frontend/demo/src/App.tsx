03b71da830798f2034a564c49b633f9fa038d730
Huaigang Liang <huaigang.liang@lumens.sg>
2025-05-21 13:41:46 +0800

    feat(demo): implement async request for BaseTable in App.tsx
    - Added an asynchronous request function to BaseTable that simulates a delay and returns a mock data structure.
    - Enhanced the component's functionality to support data fetching.

--- apps/frontend/demo/src/App.tsx
+++ apps/frontend/demo/src/App.tsx
@@ -7,6 +7,16 @@ function App() {
         cacheKey="sd"
         searchColumns={[{ title: "demo", dataIndex: ["demo"] }]}
         columns={[{ title: "haa", dataIndex: ["sd"] }]}
+        request={async () => {
+          await new Promise((resolve) => {
+            setTimeout(resolve, 200);
+          });
+          return {
+            data: [],
+            success: true,
+            total: 100,
+          };
+        }}
       ></BaseTable>
     </InitProConfig>
   );
