c5ff49992e037cb19d6fe4697e234e9837e68804
Huaigang Liang <huaigang.liang@lumens.sg>
2025-08-21 09:37:29 +0800

    refactor(AI demo): enhance canvas background and layout for improved user experience
    - Added a semi-transparent background image to the CarDamageCanvas for better visual context.
    - Wrapped the CarDamageCanvas component in a relative div to manage layout and positioning more effectively.
    - Maintained existing functionality for pin addition and interaction while improving overall aesthetics.

--- apps/frontend/demo/src/app/ai-demo/page.tsx
+++ apps/frontend/demo/src/app/ai-demo/page.tsx
@@ -28,37 +28,45 @@ export default function Page() {
           获取图像
         </Button>
       </div>
-      <CarDamageCanvas
-        ref={damageCanvasIns}
-        pins={pins}
-        onAdd={(ev: any, type: string) => {
-          setPins((prev) => {
-            return [
-              ...prev,
-              {
-                position: {
-                  x: ev.x,
-                  y: ev.y,
+      <div className="relative inline-block">
+        <div
+          className="absolute w-full h-full opacity-30"
+          style={{
+            background: `url("/assets/image.png") left top / 100% 100% no-repeat`,
+          }}
+        ></div>
+        <CarDamageCanvas
+          ref={damageCanvasIns}
+          pins={pins}
+          onAdd={(ev: any, type: string) => {
+            setPins((prev) => {
+              return [
+                ...prev,
+                {
+                  position: {
+                    x: ev.x,
+                    y: ev.y,
+                  },
+                  metaData: {
+                    type,
+                  },
                 },
-                metaData: {
-                  type,
-                },
-              },
-            ];
-          });
-        }}
-        onClickPin={(data: any) => {
-          modal.info({
-            title: data?.metaData?.type ?? "sdfdsf",
-            content: (
-              <div>
-                <div>x: {data?.position?.x}</div>
-                <div>y: {data?.position?.y}</div>
-              </div>
-            ),
-          });
-        }}
-      />
+              ];
+            });
+          }}
+          onClickPin={(data: any) => {
+            modal.info({
+              title: data?.metaData?.type ?? "sdfdsf",
+              content: (
+                <div>
+                  <div>x: {data?.position?.x}</div>
+                  <div>y: {data?.position?.y}</div>
+                </div>
+              ),
+            });
+          }}
+        />
+      </div>
 
       {imageData && <img src={imageData} alt="demo" />}
     </div>
